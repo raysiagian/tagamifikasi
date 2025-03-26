c19827552a33ba1340355e5d4edef0597c5060db
Gunawan <gsinaga53@gmail.com>
2025-03-26 09:35:39 +0700

    push

--- mobile/vak_app/lib/models/level.dart
+++ mobile/vak_app/lib/models/level.dart
@@ -1,4 +1,4 @@
-class Level{
+class Level {
   final int id_level;
   final int id_mataPelajaran;
   final String penjelasan_level;
@@ -9,16 +9,17 @@ class Level{
     required this.penjelasan_level,
   });
 
-  factory Level.fromJson(Map<String,dynamic>json){
+  factory Level.fromJson(Map<String, dynamic> json) {
     return Level(
-      id_level: json['id_level'], 
-      id_mataPelajaran: json['id_mataPelajaran'], 
+      id_level: json['id_level'],
+      id_mataPelajaran: json['id_mataPelajaran'],
       penjelasan_level: json['penjelasan_level'],
     );
   }
 }
 
-List<Level>level = [
+List<Level> level = [
   Level(id_level: 1, id_mataPelajaran: 1, penjelasan_level: 'pengenalan angka'),
-  Level(id_level: 2, id_mataPelajaran: 1, penjelasan_level: 'pengenalan angka 2'),
-];  
+  Level(
+      id_level: 2, id_mataPelajaran: 1, penjelasan_level: 'pengenalan angka 2'),
+];
--- mobile/vak_app/lib/screen/homeScreen/main/homeScreen.dart
+++ mobile/vak_app/lib/screen/homeScreen/main/homeScreen.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:vak_app/screen/stageScreen/main/stageScreen.dart';
+
 import 'package:vak_app/style/localColor.dart';
+
+import '../../../models/mataPelajaran.dart';
+import '../../../services/matapelajaran_Services.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -10,39 +14,70 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final MataPelajaranService _mataPelajaranService = MataPelajaranService();
+
   @override
   Widget build(BuildContext context) {
-    final screenWidth = MediaQuery.of(context).size.width;
-
     return SafeArea(
       child: Scaffold(
         body: Container(
           decoration: const BoxDecoration(
             image: DecorationImage(
-              image: AssetImage("assets/images/background/HiFi-Home Background.png"),
+              image: AssetImage(
+                  "assets/images/background/HiFi-Home Background.png"),
               fit: BoxFit.cover,
             ),
           ),
-          child: Stack(
-            children: [
-              Container(
-                width: double.infinity,
-                height: MediaQuery.of(context).size.height,
-                color: LocalColor.transparent,
-              ),
-              _buildSubjectIcon(top: 290, left: 80, imagePath: "assets/images/component/HiFi-Komunikasi Subject Icon.png"),
-              _buildSubjectIcon(top: 340, right: 20, imagePath: "assets/images/component/HiFi-Bahasa Indonesia Subject Icon.png"),
-              _buildSubjectIcon(bottom: 160, left: 20, imagePath: "assets/images/component/HiFi-Bahasa Inggris Subject Icon.png"),
-              _buildSubjectIcon(bottom: 20, left: 80, imagePath: "assets/images/component/HiFi-Matematika Subject Icon.png"),
-              _buildSubjectIcon(bottom: 120, right: 50, imagePath: "assets/images/component/HiFi-Sains Subject Icon.png"),
-            ],
+          child: FutureBuilder<List<MataPelajaran>>(
+            future: _mataPelajaranService.fetchMataPelajaran(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Center(child: CircularProgressIndicator());
+              } else if (snapshot.hasError) {
+                return Center(child: Text("Error: ${snapshot.error}"));
+              } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                return const Center(
+                    child: Text("Tidak ada data mata pelajaran"));
+              }
+
+              List<MataPelajaran> mataPelajaranList = snapshot.data!;
+              List<Widget> subjectIcons = [];
+
+              List<Map<String, dynamic>> iconPositions = [
+                {"top": 290.0, "left": 80.0},
+                {"top": 340.0, "right": 20.0},
+                {"bottom": 160.0, "left": 20.0},
+                {"bottom": 20.0, "left": 80.0},
+                {"bottom": 120.0, "right": 50.0},
+              ];
+
+              for (int i = 0; i < mataPelajaranList.length; i++) {
+                MataPelajaran mataPelajaran = mataPelajaranList[i];
+                subjectIcons.add(
+                  _buildSubjectIcon(
+                    top: iconPositions[i]["top"],
+                    bottom: iconPositions[i]["bottom"],
+                    left: iconPositions[i]["left"],
+                    right: iconPositions[i]["right"],
+                    imagePath: mataPelajaran.iconPath,
+                  ),
+                );
+              }
+
+              return Stack(children: subjectIcons);
+            },
           ),
         ),
       ),
     );
   }
 
-  Widget _buildSubjectIcon({double? top, double? bottom, double? left, double? right, required String imagePath}) {
+  Widget _buildSubjectIcon(
+      {double? top,
+      double? bottom,
+      double? left,
+      double? right,
+      required String imagePath}) {
     return Positioned(
       top: top,
       bottom: bottom,
