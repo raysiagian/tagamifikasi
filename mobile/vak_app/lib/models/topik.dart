850ab3c555fe0c351a9dc695a4c50476ba33690d
raysiagian <anggasiagian17@gmail.com>
2025-06-24 14:19:21 +0700

    inisiasi topik perbaikan model

--- mobile/vak_app/lib/models/topik.dart
+++ mobile/vak_app/lib/models/topik.dart
@@ -5,6 +5,7 @@ class Topik {
   final String penjelasan_level;
 
   Topik({
+    required this.id_topik,
     required this.id_level,
     // required this.id_mataPelajaran,
     required this.penjelasan_level,
@@ -12,6 +13,7 @@ class Topik {
 
   factory Topik.fromJson(Map<String, dynamic> json) {
     return Topik(
+      id_topik: json['id_topik'],
       id_level: json['id_level'],
       // id_mataPelajaran: json['id_mataPelajaran'],
       penjelasan_level: json['penjelasan_level'],
